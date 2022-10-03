5ddcc245326bfdeeeb4dbf549191e9d286403e1a
vikivero <vikichan.ch@gmail.com>
2022-10-03 22:17:35 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -4,4 +4,10 @@ function avg($str)
     $nums = explode(', ', $str);
 	echo array_sum($nums)/count($nums);
 }
+
+function avg_wazona($str)
+{
+    $nums = explode(', ', $str);
+	echo array_sum($nums)/count($nums);
+}
 ?>
